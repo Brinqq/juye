bd15285faba11e2cec941ba9401f0bcf50e2d4ce
brinq <andrewrdavis18@gmail.com>
2025-11-30 18:54:55 -0800

    more input

---
+++ src/gk/keyboard.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#if __APPLE__
+#include "GLFW/glfw3.h"
+
+const int PlatformKeyMap[] = {
+  GLFW_KEY_W,
+  GLFW_KEY_A,
+  GLFW_KEY_S,
+  GLFW_KEY_D,
+  GLFW_MOUSE_BUTTON_1,
+  GLFW_MOUSE_BUTTON_2,
+  GLFW_KEY_LEFT_SHIFT,
+  GLFW_KEY_SPACE
+};
+
+#endif
+
+#define _ssf_translate_key(_key) PlatformKeyMap[_key]
+
+namespace ssf{
+  enum KeyCode{
+    KeyCodeNone = -1,
+    KeyCodeW = 0,
+    KeyCodeA = 1,
+    KeyCodeS = 2,
+    KeyCodeD = 3,
+    KeyCodeLMB = 4,
+    KeyCodeRMB = 5,
+    KeyCodeSHIFT = 6,
+    KeyCodeSPACE = 7,
+  };
+};
