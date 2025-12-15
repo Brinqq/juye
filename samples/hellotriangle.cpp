48fb146b7990f116137e6689f3250aa572f7ffa0
brinq <andrewrdavis18@gmail.com>
2025-12-15 04:13:18 -0800

    starting samples

---
+++ samples/hellotriangle.cpp
@@ -0,0 +1,12 @@
+#include "juye/juye.h"
+#include "juye/prefabs.h"
+
+#include <stdio.h>
+
+int main(){
+
+ // auto cube =  juye::Prefab::Builder()
+ //       .SetMesh(juye::Prefab::PrefabMeshCube);
+
+  printf("here\n");
+}
