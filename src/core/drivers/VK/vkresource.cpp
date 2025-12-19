b5f950c5677f43e75166e895e673f8a3e83e352a
brinq <andrewrdavis18@gmail.com>
2025-12-18 20:30:14 -0800

    renderpass

--- src/core/drivers/VK/vkresource.cpp
+++ src/core/drivers/VK/vkresource.cpp
@@ -1,6 +1,149 @@
 #include "vkresource.h"
+#include "core/global.h"
 
 using namespace juye::driver;
+
+
+  RenderPassBuilder RenderPassBuilder::AddDepthAttachment(VkFormat format){
+    hasDepthBuf = true;
+
+    VkAttachmentDescription desc;
+    desc.flags = 0;
+    desc.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    desc.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    desc.finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+    desc.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    desc.format = format;
+    desc.samples = VK_SAMPLE_COUNT_1_BIT;
+    desc.loadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    desc.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    attachments[numAttachments] = desc;
+    depthRef = VkAttachmentReference{static_cast<uint32_t>(numAttachments),  VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL};
+    numAttachments++;
+    return *this;
+  }
+
+  RenderPassBuilder RenderPassBuilder::AddAttachment(const VkFormat format,const VkImageLayout initialLayout, 
+                    const VkImageLayout gpuReadyLayout, uint32_t flags){
+
+    VkAttachmentDescription desc;
+    desc.flags = 0;
+    desc.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    desc.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    desc.finalLayout = gpuReadyLayout;
+    desc.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    desc.format = format;
+    desc.samples = VK_SAMPLE_COUNT_1_BIT;
+    desc.loadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    desc.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    
+    if(flags & AttachmentCreateClearOnLoad){
+      desc.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    }else if(flags & AttachmentCreateKeepOnLoad){
+      desc.loadOp = VK_ATTACHMENT_LOAD_OP_LOAD;
+    }
+
+    if(flags & AttachmentCreateKeepOnStore){ desc.storeOp = VK_ATTACHMENT_STORE_OP_STORE; }
+
+    attachments[numAttachments] = desc;
+    numAttachments++;
+
+    return *this;
+  }
+
+  RenderPassBuilder& RenderPassBuilder::BeginSubpass(){
+    auto& pass = subpasses.at(numSubpasses);
+    auto& desc = subpassHandles.at(numSubpasses);
+    desc.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    desc.flags = 0;
+    desc.pResolveAttachments = nullptr;
+    desc.pDepthStencilAttachment = nullptr;
+    pass.numWriteAttachmets = 0;
+    pass.numReadAttachmets = 0;
+    pass.numPreserveAttachmets = 0;
+
+    if(hasDepthBuf){
+      desc.pDepthStencilAttachment = &depthRef;
+    }
+
+    return *this;
+  }
+
+  RenderPassBuilder& RenderPassBuilder::SetReadAttachment(uint32_t index){
+    auto& pass = subpasses.at(numSubpasses);
+    auto& attachment = attachments[index];
+    VkAttachmentReference ref{index, attachment.initialLayout};
+    pass.read[pass.numReadAttachmets] = ref;
+    pass.numReadAttachmets++;
+    return *this;
+  }
+
+  RenderPassBuilder& RenderPassBuilder::SetWriteAttachment(uint32_t index){
+    auto& pass = subpasses.at(numSubpasses);
+    auto& attachment = attachments[index];
+    VkAttachmentReference ref{index, attachment.initialLayout};
+    pass.write[pass.numWriteAttachmets] = ref;
+    pass.numWriteAttachmets++;
+    return *this;
+  }
+
+  
+  RenderPassBuilder& RenderPassBuilder::SetPreserveAttachment(uint32_t index){
+    auto& pass = subpasses.at(numSubpasses);
+    auto& attachment = attachments[index];
+    VkAttachmentReference ref{index, attachment.initialLayout};
+    pass.preserve[pass.numPreserveAttachmets] = index;
+    pass.numPreserveAttachmets++;
+    return *this;
+  }
+
+  RenderPassBuilder& RenderPassBuilder::EndSubpass(){
+    auto& pass = subpasses.at(numSubpasses);
+    auto& desc = subpassHandles.at(numSubpasses);
+    desc.pInputAttachments = pass.read;
+    desc.pColorAttachments = pass.write;
+    desc.pPreserveAttachments = pass.preserve;
+
+    desc.inputAttachmentCount = pass.numReadAttachmets;
+    desc.colorAttachmentCount = pass.numWriteAttachmets;
+    desc.preserveAttachmentCount = pass.numPreserveAttachmets;
+    numSubpasses++;
+
+    return *this;
+  }
+
+  RenderPassBuilder& RenderPassBuilder::Reset(){
+    return *this;
+  }
+
+  VkRenderPass RenderPassBuilder::Build(VkDevice device){
+    
+  VkSubpassDependency dependency{};
+  dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
+  dependency.dstSubpass = 0;
+  dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
+  dependency.srcAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+  dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
+  dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+  
+    VkRenderPass ret;
+    VkRenderPassCreateInfo c;
+
+    c.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    c.pNext = nullptr;
+    c.flags = 0;
+    c.attachmentCount = numAttachments;
+    c.pAttachments = attachments.data();
+    c.subpassCount = numSubpasses;
+    c.pSubpasses = subpassHandles.data();
+    c.dependencyCount = 1;
+    c.pDependencies = &dependency;
+
+    vkcall(vkCreateRenderPass(device, &c, nullptr, &ret))
+    return ret;
+
+  }
+
 
 DescriptorSetLayoutBuilder& DescriptorSetLayoutBuilder::AddBinding(uint32_t idx, uint32_t batch,
                           const VkDescriptorType type, const VkShaderStageFlags stages) noexcept{
--- src/core/drivers/VK/vkresource.h
+++ src/core/drivers/VK/vkresource.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "vkdefines.h"
 
+#include <array>
+#include <vector>
+
 namespace juye::driver{
 
 enum ResourceType{
@@ -8,6 +11,66 @@ enum ResourceType{
   ResourceUniform
 };
 
+
+class RenderPassBuilder{
+private:
+  static constexpr int kMaxAttachments = 5;
+  static constexpr int kMaxSubpasses = 5;
+
+public:
+  enum AttachmentUsageType{
+    AttachmentUsageRead,
+    AttachmentUsageWrite,
+  };
+
+  enum AttachmentCreateFlags{
+    AttachmentCreateNone = 0x0,
+    AttachmentCreateClearOnLoad = 0x1,
+    AttachmentCreateKeepOnLoad = 0x4,
+    AttachmentCreateKeepOnStore = 0x8,
+    AttachmentCreateClearOnStore = 0x8,
+  };
+private:
+
+  struct Subpass{
+    VkAttachmentReference write[kMaxAttachments];
+    int numWriteAttachmets;
+    VkAttachmentReference read[kMaxAttachments];
+    int numReadAttachmets;
+    uint32_t preserve[kMaxAttachments];
+    int numPreserveAttachmets;
+  };
+
+  struct Attachment{
+    VkFormat format;
+    VkImageLayout layout;
+  };
+
+  std::array<VkAttachmentDescription, kMaxAttachments> attachments;
+  std::array<VkSubpassDescription, kMaxSubpasses> subpassHandles;
+  std::array<Subpass, kMaxSubpasses> subpasses;
+
+  int numSubpasses  = 0;
+  int numAttachments  = 0;
+  bool hasDepthBuf = false;
+  VkAttachmentReference depthRef;
+public:
+
+
+  RenderPassBuilder AddDepthAttachment(VkFormat format);
+  RenderPassBuilder AddAttachment(const VkFormat format,const VkImageLayout initialLayout, 
+                    const VkImageLayout gpuReadyLayout, uint32_t flags);
+
+  RenderPassBuilder& BeginSubpass();
+  RenderPassBuilder& SetReadAttachment(uint32_t index);
+  RenderPassBuilder& SetWriteAttachment(uint32_t index);
+  RenderPassBuilder& SetPreserveAttachment(uint32_t index);
+  RenderPassBuilder& EndSubpass();
+  RenderPassBuilder& Reset();
+
+  VkRenderPass Build(VkDevice device);
+
+};
 
 class DescriptorSetLayoutBuilder{
 private:
