11ca13a29beee83aaa9cf30b5cda6210042be131
Brinq q <andrewrdavis18@gmail.com>
2026-01-10 23:56:33 -0800

    changed name

---
+++ src/core/drivers/VK/shader.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <stdint.h>
+
+#include <vulkan/vulkan.h>
+
+#include <vector>
+
+constexpr int kMaxNameLength = 10;
+typedef void* ShaderHandle;
+
+struct ShaderRequirements{
+
+};
+
+struct ShaderResources{
+  std::vector<VkPipelineShaderStageCreateInfo> shaders;
+  std::vector<VkVertexInputBindingDescription> inputs;
+  std::vector<VkVertexInputAttributeDescription> attributes;
+  std::vector<VkPushConstantRange> pushRanges;
+};
+
+struct ShaderContainer{
+  ShaderResources resources;
+  ShaderRequirements requirements;
+  ShaderHandle handle;
+};
+
+ShaderContainer  BuildShaderFromMetaFile(VkDevice device, VkAllocationCallbacks* allocator, const char* pMetaFile);
+void ShaderFreeContainer(VkDevice device, VkAllocationCallbacks*, ShaderContainer* container);
+
