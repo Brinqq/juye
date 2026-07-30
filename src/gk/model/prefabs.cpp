896d3c8b2f25190d680078aba7387ce06183fee5
brinq <andrewrdavis18@gmail.com>
2026-07-29 17:56:02 -0700

    prefab no longer is front facing

--- src/gk/model/prefabs.cpp
+++ src/gk/model/prefabs.cpp
@@ -1,20 +1,20 @@
-#include "juye/gk/prefabs.h"
+#include "prefabs.h"
 
 using namespace juye;
 
-static float kPlaneVertices[32] = {
+static float k_plane_vertices[32] = {
   -0.5f, 0.0f, -0.5f,   0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
    0.5f, 0.0f, -0.5f,   1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
    0.5f, 0.0f,  0.5f,   1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
   -0.5f, 0.0f,  0.5f,   0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
 };
 
-static uint16_t kPlaneIndices[6] = {
+static uint16_t k_plane_indices[6] = {
   0, 1, 2, 2, 3, 0
 };
 
 
-static float kCubeVertices[192] = {  // 24 vertices × 5 floats
+static float k_cube_vertices[192] = {  // 24 vertices × 5 floats
   -0.5f, -0.5f,  0.5f,   0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
    0.5f, -0.5f,  0.5f,   1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
    0.5f,  0.5f,  0.5f,   1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
@@ -47,7 +47,7 @@ static float kCubeVertices[192] = {  // 24 vertices × 5 floats
 };
 
 
-static  uint16_t kCubeIndices[36] = {
+static  uint16_t k_cube_indices[36] = {
     0,  1,  2,   2,  3,  0,   
     4,  5,  6,   6,  7,  4,   
     8,  9, 10,  10, 11,  8,   
@@ -57,35 +57,33 @@ static  uint16_t kCubeIndices[36] = {
 };
 
 
-Prefab Prefab::Builder::Build() noexcept{
+Prefab Prefab::Builder::build() noexcept{
   Prefab ret;
-  ret.mesh.pVertices = this->pVertices;
-  ret.mesh.pIndices = this->pIndices;
-  ret.mesh.numIndices = this->indices;
-  ret.mesh.numVertices = this->vertices;
-  ret.mesh.stride = this->stride;
+  ret.vertices = vertices;
+  ret.indices = indices;
+  ret.n_indices = n_indices;
+  ret.n_vertices = n_vertices;
+  ret.stride = stride;
   return ret;
 };
 
-Prefab::Builder& Prefab::Builder::SetMesh(const Prefab::BuiltinMeshType type) noexcept{
+Prefab::Builder& Prefab::Builder::set_mesh(const Prefab::BuiltinMeshType type) noexcept{
   switch (type){
-
     case PrefabMeshCube:
-    this->pIndices = kCubeIndices;
-    this->pVertices = kCubeVertices;
-    this->indices = 36;
-    this->vertices = 24;
+    this->indices = k_cube_indices;
+    this->vertices = k_cube_vertices;
+    this->n_indices = 36;
+    this->n_vertices = 24;
     this->stride = sizeof(float) * 8;
     return *this;
 
     case PrefabMeshPlane:
-    this->indices = 6;
-    this->pIndices = kPlaneIndices;
-    this->vertices = 4;
+    this->vertices = k_plane_vertices;
+    this->indices = k_plane_indices;
+    this->n_indices = 6;
+    this->n_vertices = 4;
     this->stride = sizeof(float) * 8;
-    this->pVertices = kPlaneVertices;
     return *this;
-
     }
 
   
