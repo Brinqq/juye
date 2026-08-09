b905c7bf1b97eafd6d0d815a9c9b0c05928bb924
brinq <andrewrdavis18@gmail.com>
2026-08-09 01:53:11 -0700

    shaders to src

---
+++ src/shaders/glsl/object.vert.glsl
@@ -0,0 +1,26 @@
+#version 450
+
+layout(location = 0) in vec3 vertex;
+layout(location = 1) in vec2 uv;
+layout(location = 2) in vec3 normal;
+
+layout(push_constant) uniform push{
+  mat4 model;
+  mat4 v;
+  mat4 p;
+};
+
+layout(set = 1, binding = 0) uniform frustum{
+  mat4 view;
+  mat4 projection;
+};
+
+layout(location = 0) out vec2 out_uv;
+layout(location = 1) out vec3 out_normal;
+
+void main(){
+
+    gl_Position =  projection * view * model * vec4(vertex, 1.0f);
+    out_uv = uv;
+    out_normal = normal;
+}
